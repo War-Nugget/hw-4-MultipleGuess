8db09f1491492e82c36336811ebefa15cd942cfd
Sahil Patel <sahildepatel@gmail.com>
2022-03-25 20:58:03 -0400

    Initial commit, writing index and declaring js variables

---
+++ script.js
@@ -0,0 +1,15 @@
+// select all elements
+const start = document.getElementById("start");
+const quiz = document.getElementById("quiz");
+const question = document.getElementById("question");
+const qImg = document.getElementById("qImg");
+const choiceA = document.getElementById("A");
+const choiceB = document.getElementById("B");
+const choiceC = document.getElementById("C");
+const choiceD = document.getElementById("D");
+const counter = document.getElementById("counter");
+const timeGauge = document.getElementById("timeGauge");
+const progress = document.getElementById("progress");
+const scoreDiv = document.getElementById("scoreContainer");
+
+
